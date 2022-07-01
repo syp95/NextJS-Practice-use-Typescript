b9821257082082b3776bebee02b7985b83a1918e
syp <sy95_055@naver.com>
2022-07-01 23:44:34 +0900

    fetch data

--- pages/api/fetchMovieData.ts
+++ pages/api/fetchMovieData.ts
@@ -1,5 +1,25 @@
 import axios from 'axios';
 
+export interface IMovie {
+    id: number;
+    backdrop_path: string;
+    poster_path: string;
+    title: string;
+    overview: string;
+    name?: string;
+}
+
+export interface IGetMoviesResult {
+    dates: {
+        maximum: string;
+        minimum: string;
+    };
+    page: number;
+    results: IMovie[];
+    total_pages: number;
+    total_results: number;
+}
+
 export const getMovie = async () => {
     const API_KEY = '10923b261ba94d897ac6b81148314a3f';
     const BASE_PATH = 'https://api.themoviedb.org/3';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import Seo from '../components/Seo';
-import { getMovie } from './api/fetchMoviedata';
+import { getMovie, IGetMoviesResult } from './api/fetchMoviedata';
 
 export default function Home() {
-    const { data, isLoading } = useQuery(['movie', 'nowPlaying'], getMovie);
+    const { data, isLoading } = useQuery<IGetMoviesResult>(
+        ['movie', 'nowPlaying'],
+        getMovie,
+    );
     console.log(data);
 
     return (
         <div>
             <Seo title='Home' />
             <h1>Hello</h1>
-            <div>{data.page}</div>
+            {isLoading && <h1>Loading...'</h1>}
+            {data?.results.map((movie) => (
+                <>
+                    <div key={movie.id}>{movie.title}</div>
+                </>
+            ))}
         </div>
     );
 }
