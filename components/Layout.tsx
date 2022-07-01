69dbac6f02edd38e0b742356e28f4a0a0ec21d7e
syp95 <sy95_055@naver.com>
2022-07-01 18:30:20 +0900

    create layout,_app

---
+++ components/Layout.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+import NavBar from './NavBar';
+
+type Props = {
+    children: ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+    return (
+        <>
+            <NavBar />
+            <div>{children}</div>
+            <footer>hi</footer>
+        </>
+    );
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,6 @@ import NavBar from '../components/NavBar';
 export default function about() {
     return (
         <div>
-            <NavBar />
             <h1>about</h1>
         </div>
     );
